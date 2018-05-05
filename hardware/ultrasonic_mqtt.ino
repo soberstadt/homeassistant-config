5e69994710d53c7ebba0b785808079f84357716d
Spencer Oberstadt <soberstadt@gmail.com>
2018-05-05 16:18:52 -0400

    check in ultrasonic sensor

---
+++ hardware/ultrasonic_mqtt.ino
@@ -0,0 +1,301 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+
+/************ WIFI and MQTT INFORMATION (CHANGE THESE FOR YOUR SETUP) ******************/
+#define wifi_ssid "SSIDNAME" //type your WIFI information inside the quotes
+#define wifi_password "wifi password"
+#define mqtt_server "mqtt.com"
+#define mqtt_user "admin" 
+#define mqtt_password "password"
+#define mqtt_port 1883
+
+
+
+/************* MQTT TOPICS (change these topics as you wish)  **************************/
+#define light_state_topic "/devices/mqtt_arrow_3/state"
+#define light_set_topic "/devices/mqtt_arrow_3/set"
+
+const char* on_cmd = "ON";
+const char* off_cmd = "OFF";
+
+/**************************** FOR OTA **************************************************/
+#define SENSORNAME "mqtt_range_sensor"
+#define OTApassword "password" // change this to whatever password you want to use when you upload OTA
+int OTAport = 8266;
+
+/**************************** PIN DEFINITIONS ********************************************/
+#define ECHOPIN    D1
+#define TRIGGERPIN D2
+
+/**************************** SENSOR DEFINITIONS *******************************************/
+float diffDIST = 5;
+float distanceValue;
+
+char message_buff[100];
+
+int calibrationTime = 0;
+
+const int BUFFER_SIZE = 300;
+
+#define MQTT_MAX_PACKET_SIZE 512
+
+bool stateOn = false;
+
+const int doorDistance = 20;
+const int floorDistance = 200;
+char previousStates[] = "eeeeeee";
+char debouncedState = 'e';
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(TRIGGERPIN, OUTPUT);
+  pinMode(ECHOPIN, INPUT);
+
+  Serial.begin(115200);
+  delay(10);
+
+  ArduinoOTA.setPort(OTAport);
+
+  ArduinoOTA.setHostname(SENSORNAME);
+
+  ArduinoOTA.setPassword((const char *)OTApassword);
+
+  Serial.print("calibrating sensor ");
+  for (int i = 0; i < calibrationTime; i++) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("Starting Node named " + String(SENSORNAME));
+
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IPess: ");
+  Serial.println(WiFi.localIP());
+  reconnect();
+}
+
+
+/********************************** START SETUP WIFI*****************************************/
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/********************************** START CALLBACK*****************************************/
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.println(message);
+
+  if (!processJson(message)) {
+    return;
+  }
+
+  sendState();
+}
+
+/********************************** START PROCESS JSON*****************************************/
+bool processJson(char* message) {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.parseObject(message);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+  return true;
+}
+
+/********************************** START SEND STATE*****************************************/
+void sendState() {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["raw_state"] = previousStates;
+  
+  if(debouncedState == 'e') {
+    root["state"] = "empty";
+  } else if(debouncedState == 'o') {
+    root["state"] = "open";
+  } else {
+    root["state"] = "car";
+  }
+
+  root["distance"] = distanceValue;
+
+
+  char buffer[root.measureLength() + 1];
+  root.printTo(buffer, sizeof(buffer));
+
+  Serial.println(buffer);
+  client.publish(light_state_topic, buffer, true);
+}
+
+/********************************** START RECONNECT*****************************************/
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(SENSORNAME, mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe(light_set_topic);
+      sendState();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+/********************************** START CHECK SENSOR **********************************/
+bool checkBoundSensor(float newValue, float prevValue, float maxDiff) {
+  return newValue < prevValue - maxDiff || newValue > prevValue + maxDiff;
+}
+
+
+void loop() {
+  ArduinoOTA.handle();
+  
+  if (!client.connected()) {
+    // reconnect();
+    software_Reset();
+  }
+  client.loop();
+
+  float newDistanceValue = measure();
+
+  if (checkBoundSensor(newDistanceValue, distanceValue, diffDIST)) {
+    distanceValue = newDistanceValue;
+    debounceState();
+    sendState();
+  }
+
+  delay(2000);
+}
+
+
+void debounceState() {
+  previousStates[6] = previousStates[5];
+  previousStates[5] = previousStates[4];
+  previousStates[4] = previousStates[3];
+  previousStates[3] = previousStates[2];
+  previousStates[2] = previousStates[1];
+  previousStates[1] = previousStates[0];
+  if(distanceValue < doorDistance) {
+    previousStates[0] = 'o';
+  } else if(distanceValue > floorDistance) {
+    previousStates[0] = 'e';
+  } else {
+    previousStates[0] = 'c';
+  }
+
+  int array[255] = {0};
+  int i, max, index;
+  for(i = 0; previousStates[i] != 0; i++)
+  {
+    ++array[previousStates[i]];
+  }
+  max = array[0];
+  index = 0;
+  for(i = 0; previousStates[i] != 0; i++)
+  {
+    if( array[previousStates[i]] > max)
+    {
+      max = array[previousStates[i]];
+      index = i;
+    }
+  }
+  debouncedState = previousStates[index];
+}
+
+
+int measure() {
+  // Clears the trigPin
+  digitalWrite(TRIGGERPIN, LOW);
+  delayMicroseconds(2);
+  
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(TRIGGERPIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIGGERPIN, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  float duration = pulseIn(ECHOPIN, HIGH);
+  
+  // Calculating the distance
+  float distance= duration*0.034/2;
+
+  return distance;
+}
+
+/****reset***/
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+{
+  Serial.print("resetting");
+  ESP.reset(); 
+}
+
