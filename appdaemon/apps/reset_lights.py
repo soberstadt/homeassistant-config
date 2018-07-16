109869219751bc901dde870eed74e93ab15bb07b
Spencer Oberstadt <soberstadt@gmail.com>
2018-07-15 23:52:05 -0400

    add some logging to reset_lights app

--- appdaemon/apps/reset_lights.py
+++ appdaemon/apps/reset_lights.py
@@ -1,6 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
 import datetime
-import time
 
 class ResetLights(hass.Hass):
   groups = ['living_room_lights', 'kitchen_lights', 'bedroom_lights']
@@ -9,10 +8,8 @@ class ResetLights(hass.Hass):
     time = datetime.time(13, 00, 0)
     self.run_daily(self.run_daily_callback, time)
 
-    # run on initialization - REMOVE BEFORE FLIGHT
-    # self.run_daily_callback(self)
-
   def run_daily_callback(self, kwargs):
+    self.log('daily callback fired')
     self.on_lights = []
 
     self.turn_on_lights()
@@ -21,17 +18,21 @@ class ResetLights(hass.Hass):
 
   # turn on the lights to 100%, recording which were already turned on
   def turn_on_lights(self):
+    self.log('lights about to turn on')
     for group_name in ResetLights.groups:
       group = self.entities.group[group_name]
       for light in group.attributes.entity_id:
         if self.get_state(light) == 'on':
           self.on_lights.append(light)
         self.turn_on(light, brightness_pct=100)
+    self.log('lights on')
 
   # turn off the lights that were not on previously
   def turn_off_lights(self, kwargs):
+    self.log('lights about to turn off')
     for group_name in ResetLights.groups:
       group = self.entities.group[group_name]
       for light in group.attributes.entity_id:
         if light not in self.on_lights:
           self.turn_off(light)
+    self.log('lights off')
