6862b7a11c37ae45f44733d09591b4de7c3d1a8e
Spencer Oberstadt <soberstadt@gmail.com>
2019-01-26 12:47:46 -0500

    code style

--- python_scripts/set_roku_volume.py
+++ python_scripts/set_roku_volume.py
@@ -1,6 +1,12 @@
 for x in range(0, 50):
-  hass.services.call('roku_remote', 'press_button', service_data={ 'button': "volume_down" }, blocking=True)
+  hass.services.call('roku_remote',
+                     'press_button',
+                     service_data={ 'button': "volume_down" },
+                     blocking=True)
 
 level = data.get('level', 15)
 for x in range(0, level):
-  hass.services.call('roku_remote', 'press_button', service_data={ 'button': "volume_up" }, blocking=True)
+  hass.services.call('roku_remote',
+                     'press_button',
+                     service_data={ 'button': "volume_up" },
+                     blocking=True)
